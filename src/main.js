1e52cc3d0ec63cbe013c2f9ad96c85644d03526d
nargesnajm <nargesnajm@gmail.com>
2025-08-10 15:52:24 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,10 +12,10 @@ router.beforeEach((to, from, next) => {
     const user = localStorage.getItem('user')
   
     if (!user && (to.path !== '/login' && to.path !== '/register')) {
-      // If not logged in and trying to access protected routes, redirect to login
+    
       next('/login')
     } else if (user && (to.path === '/login' || to.path === '/register')) {
-      // If logged in and trying to access login/register, redirect to objects list
+ 
       next('/objects')
     } else {
       next()
